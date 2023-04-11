d50c5ac7bd18dc7cbdb6fe0c312a2f65145c320b
Carl <carl.emmoth@gmail.com>
2023-04-11 18:04:15 +0200

    WebSocket security

---
+++ src/main/java/ce/security/WebSocketSecurityConfig.java
@@ -0,0 +1,26 @@
+package ce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig {
+
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+        return messages
+                .anyMessage().permitAll()
+                .nullDestMatcher().permitAll()
+                .simpDestMatchers("**").permitAll()
+                .simpMessageDestMatchers("**").permitAll()
+                .simpSubscribeDestMatchers("**").permitAll()
+                .build()
+                ;
+    }
+}
