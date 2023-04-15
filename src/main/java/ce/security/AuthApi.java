0cdb6ddb0d74dedd3649249094f6f64f67004f5f
Carl <carl.emmoth@gmail.com>
2023-04-15 15:45:21 +0200

    Updating to be in sync with other branch

--- src/main/java/ce/security/AuthApi.java
+++ src/main/java/ce/security/AuthApi.java
@@ -1,11 +1,9 @@
 package ce.security;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
 public class AuthApi {
 
   @GetMapping("/api/v1/public/")
--- src/main/java/ce/security/SecurityConfig.java
+++ src/main/java/ce/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,13 +11,6 @@ import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,27 +23,18 @@ public class SecurityConfig {
   @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
   private String issuer;
 
-//  @Value("${cors}")
-//  private String cors;
-
-//  @Value("${csp}")
-//  private String csp;
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests()
-        .requestMatchers("/api/v1/public/**", "/", "/index.html", "/favicon.ico", "/auth_config.json", "/web-socket/").permitAll()
-        .requestMatchers("/api/v1/private/**").authenticated()
+    http
+        .authorizeHttpRequests()
+        .requestMatchers("/api/v1/public/**", "/", "/index.html", "/favicon.ico", "/auth_config.json", "/web-socket/"
+        ).permitAll()
+        .requestMatchers("/api/v1/private/**", "/server-time", "/multiply", "/immutable", "/row", "/user/**").authenticated()
         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and().oauth2ResourceServer().jwt()
         ;
     return http.build();
   }
-
-//  @Override
-//  public void addCorsMappings(CorsRegistry registry) {
-//    registry.addMapping("/**").allowedMethods("*");
-//  }
 
   @Bean
   JwtDecoder jwtDecoder() {
@@ -66,26 +49,4 @@ public class SecurityConfig {
 
       return jwtDecoder;
   }
-
-//  private CorsConfigurationSource corsConfigurationSource() {
-//    final CorsConfiguration configuration = new CorsConfiguration();
-//    configuration.setAllowedMethods(Arrays.asList(
-//      HttpMethod.GET.name(),
-//      HttpMethod.PUT.name(),
-//      HttpMethod.POST.name(),
-//      HttpMethod.DELETE.name()
-//    ));
-//
-//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//    source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
-//    return source;
-//  }
-
-//  @Bean
-//  SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
-//    http.headers().contentSecurityPolicy("default-src 'self' " + csp)
-//      .reportOnly(true)
-//      ;
-//    return http.build();
-//  }
 }
